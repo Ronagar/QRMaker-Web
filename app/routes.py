6227f6b53589e77fffb42d4bb8f7c8defe2077e2
Ronagar <rnginformatica@uma.es>
2024-01-29 12:53:36 +0100

    Funcionalidad básica creada correctamente

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, request, send_file
 from app import app
+from createQR import generate_qr_codes, createZip 
+import os
+import shutil
 
 
 @app.route('/')
@@ -19,13 +22,21 @@ def generate():
         return redirect(request.url)
     
     #save the uploaded csv file 
-    csv_file.save('uploads/' + csv_file.filename)
+    csv_path = 'uploads/' + csv_file.filename
+    csv_file.save(csv_path)
 
     #Generate QR codes
-
+    tickets = int(request.form['tickets'])
+    generate_qr_codes(csv_path, tickets)
 
     #Create ZIP file 
-    zip_path = "path del zip"
+    zip_path = '../'+createZip('uploads/')
+    print(zip_path)
+
+    #Remove the uploaded CSV file and the generated QR code images
+    os.remove(csv_path)
+    qrImagesPath = 'uploads/' + 'qr_images'
+    shutil.rmtree(qrImagesPath)
 
     #Send the zip file to the user
     return send_file(zip_path, as_attachment=True, download_name='qrcodes.zip')
--- createQR.py
+++ createQR.py
@@ -1,51 +1,57 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 import qrcode
-from PIL import Image
+#from PIL import Image
 import csv 
+import zipfile
+import os
 
-imagenlogo = './etsii.jpeg'
-logo = Image.open(imagenlogo)
-# Resize the image
-hsize = int((float(logo.size[1])*float(60/float(logo.size[0]))))
-#logo = logo.resize((100, hsize), Image.ANTIALIAS)
-logo = logo.resize((60, hsize))
+def generate_qr_codes(csvPath, ntickets):
+    print("he entrado en generar qrs")
+    #csvPath = 'uploads/alumns.csv'
+    with open(csvPath, encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        headers = [header.strip() for header in reader.fieldnames]
+        for row in reader:
+            #store the attendant full name writing '_' instead of spaces
+            name = row.get(headers[0]).replace(' ', '_')
+            surnames = row.get(headers[1]).replace(' ', '_')
+            attendant = surnames+'_'+name
+            
 
-with open('alumnos.csv', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    headers = [header.strip() for header in reader.fieldnames]
-    for row in reader:
-        #store the student full name writing '_' instead of spaces
-        name = row.get(headers[0]).replace(' ', '_')
-        surnames = row.get(headers[1]).replace(' ', '_')
-        student = surnames+'_'+name
+            #Creation of the path if neccessary
+            Path('uploads/qr_images/'+attendant).mkdir(parents=True, exist_ok=True)
+            
+            for n in range(ntickets):
+                QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+                #extract the attendant id from the dataSet
+                data = row.get(headers[2]) + row.get(headers[3])
+                #Feed the QR Code data
+                data = data+'_'+str(n+1)
+                QRcode.add_data(data)
+                QRcode.make(fit=True)
+                # Insert the image to the QR code and create the final image
+                QRimg = QRcode.make_image(fill_color='Black', back_color='White').convert('RGB')
+
+                #We define the image position (center)
+                #pos = ((QRimg.size[0] - logo.size[0]) // 2,(QRimg.size[1] - logo.size[1]) // 2)
+                #QRimg.paste(logo, pos)
+            
+                #Save the image .png
+                path = 'uploads/qr_images/'+attendant+'/'+data+'.png'
+                QRimg.save(path)
         
-
-        #Creation of the path if neccessary
-        Path('./outputs/'+student).mkdir(parents=True, exist_ok=True)
-        
-
-        for letter in ('a','b','c'):
-            QRcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-            #extract the student id from the dataSet
-            data = row.get(headers[2])
-            #Feed the QR Code data
-            data = data+'_'+letter
-            QRcode.add_data(data)
-            QRcode.make
-            # Insert the image to the QR code and create the final image
-            QRimg = QRcode.make_image(fill_color='Black', back_color='White').convert('RGB')
-
-            #We define the image position (center)
-            pos = ((QRimg.size[0] - logo.size[0]) // 2,(QRimg.size[1] - logo.size[1]) // 2)
-            QRimg.paste(logo, pos)
-        
-            #Save the image .png
-            path = './outputs/'+student+'/'+data+'.png'
-            QRimg.save(path)
-     
-file.close()
+    file.close()
 
 
-
+def createZip(Path):
+    zipPath = os.path.join(Path+'codes.zip')
+    with zipfile.ZipFile(zipPath, 'w') as myZip:
+        # Recorre los archivos en qr_images y los añade al archivo ZIP
+        for root, dirs, files in os.walk(os.path.join(Path, 'qr_images')):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, os.path.join(Path, 'qr_images'))
+                myZip.write(file_path, arcname)
+    return zipPath
 
